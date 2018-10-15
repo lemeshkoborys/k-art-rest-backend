45c01b5ad17eec5509e7f6d96bf919b06693a589
Borys Lemeshko <lemeshko.borys@gmail.com>
2018-10-15 14:13:13 +0300

    Issue #1 Done. Prepearing for demo deploy

--- k_art_rest/settings.py
+++ k_art_rest/settings.py
@@ -155,6 +155,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 import dj_database_url
